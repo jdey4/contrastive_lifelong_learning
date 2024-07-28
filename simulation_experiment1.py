e33c339c45cc0702a1737fbd74308b87e64d45e1
jdey4 <jdey4@jhmi.edu>
2024-07-28 19:12:45 -0400

    slurm commands

--- simulation_experiment1.py
+++ simulation_experiment1.py
@@ -20,6 +20,88 @@ def generate_task_data(sample_size, angle, latent_dim, noise_dim):
 
     return X, y
 
+def generate_gaussian_parity(
+    n_samples,
+    centers=None,
+    class_label=None,
+    cluster_std=0.25,
+    bounding_box=(-1.0, 1.0),
+    angle_params=None,
+    random_state=None,
+):
+    """
+    Generate 2-dimensional Gaussian XOR distribution.
+    (Classic XOR problem but each point is the
+    center of a Gaussian blob distribution)
+    Parameters
+    ----------
+    n_samples : int
+        Total number of points divided among the four
+        clusters with equal probability.
+    centers : array of shape [n_centers,2], optional (default=None)
+        The coordinates of the ceneter of total n_centers blobs.
+    class_label : array of shape [n_centers], optional (default=None)
+        class label for each blob.
+    cluster_std : float, optional (default=1)
+        The standard deviation of the blobs.
+    bounding_box : tuple of float (min, max), default=(-1.0, 1.0)
+        The bounding box within which the samples are drawn.
+    angle_params: float, optional (default=None)
+        Number of radians to rotate the distribution by.
+    random_state : int, RandomState instance, default=None
+        Determines random number generation for dataset creation. Pass an int
+        for reproducible output across multiple function calls.
+    Returns
+    -------
+    X : array of shape [n_samples, 2]
+        The generated samples.
+    y : array of shape [n_samples]
+        The integer labels for cluster membership of each sample.
+    """
+
+    if random_state != None:
+        np.random.seed(random_state)
+
+    if centers == None:
+        centers = np.array([(-0.5, 0.5), (0.5, 0.5), (-0.5, -0.5), (0.5, -0.5)])
+
+    if class_label == None:
+        class_label = [0, 1, 1, 0]
+
+    blob_num = len(class_label)
+
+    # get the number of samples in each blob with equal probability
+    samples_per_blob = np.random.multinomial(
+        n_samples, 1 / blob_num * np.ones(blob_num)
+    )
+
+    X = np.zeros((1,2), dtype=float)
+    y = np.zeros((1), dtype=float)
+    ii = 0
+    for center, sample in zip(centers, samples_per_blob):
+        X_, _ = make_blobs(
+            n_samples=sample*10,
+            n_features=2,
+            centers=[center],
+            cluster_std=cluster_std
+        )
+        col1 = (X_[:,0] > bounding_box[0]) & (X_[:,0] < bounding_box[1])
+        col2 = (X_[:,1] > bounding_box[0]) & (X_[:,1] < bounding_box[1])
+        X_ = X_[col1 & col2]
+        X = np.concatenate((X,X_[:sample,:]), axis=0)
+        y_ = np.array([class_label[ii]]*sample)
+        y = np.concatenate((y, y_), axis=0)
+        ii += 1
+
+    X, y = X[1:], y[1:]
+
+    if angle_params != None:
+        R = _generate_2d_rotation(angle_params)
+        X = X @ R
+
+    return X, y.astype(int)
+
+#%%
 class TaskDataset(Dataset):
     def __init__(self, X, y):
         self.X = X.clone()
